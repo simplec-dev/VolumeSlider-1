7f86ab2f4160ecc667ecc0c2d699c53bb4d15ff6
doug.nelson <dnelson@simplec.com>
2015-08-22 16:07:33 -0400

    .

--- src/android/com/simplec/phonegap/plugins/volumeslider/VolumeSlider.java
+++ src/android/com/simplec/phonegap/plugins/volumeslider/VolumeSlider.java
@@ -8,6 +8,7 @@ import org.json.JSONException;
 import android.graphics.Color;
 import android.graphics.drawable.ShapeDrawable;
 import android.graphics.drawable.shapes.OvalShape;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager.LayoutParams;
 import android.widget.AbsoluteLayout;
@@ -18,19 +19,32 @@ public class VolumeSlider extends CordovaPlugin {
 	private static final String CREATE_SLIDER = "createVolumeSlider";
 	private static final String SHOW_SLIDER = "showVolumeSlider";
 	private static final String HIDE_SLIDER = "hideVolumeSlider";
+	
+	private SeekBar seekBar = null;
 
 	@Override
 	public boolean execute(String action, JSONArray data, final CallbackContext callbackContext) throws JSONException {
 		try {
 			if (CREATE_SLIDER.equals(action)) {
+				Log.e("VolmeSlider", "creating slider execute");
+				createSlider(data);
 				return true;
-
 			} else if (SHOW_SLIDER.equals(action)) {
+				Log.e("VolmeSlider", "show slider execute");
+				if (seekBar!=null) {
+					Log.e("VolmeSlider", "setting visible");
+					seekBar.setVisibility(View.VISIBLE);
+				}
+				
 				return true;
-
 			} else if (HIDE_SLIDER.equals(action)) {
+				Log.e("VolmeSlider", "hide slider execute");
+				if (seekBar!=null) {
+					Log.e("VolmeSlider", "setting invisible");
+					seekBar.setVisibility(View.INVISIBLE);
+				}
+				
 				return true;
-
 			} else {
 				callbackContext.error(action + " is not a supported function.");
 				return false;
@@ -48,41 +62,48 @@ public class VolumeSlider extends CordovaPlugin {
 			int width = data.getInt(2);
 			int height = data.getInt(3);
 
-			SeekBar seekBar = new SeekBar(webView.getContext());
-			seekBar.setMax(100);
-			// seekBar.setIndeterminate(true);
+			if (seekBar==null) {
+				seekBar = new SeekBar(webView.getContext());
+				seekBar.setMax(100);
+				// seekBar.setIndeterminate(true);
 
-			ShapeDrawable thumb = new ShapeDrawable(new OvalShape());
+				ShapeDrawable thumb = new ShapeDrawable(new OvalShape());
 
-			thumb.setIntrinsicHeight(height);
-			thumb.setIntrinsicWidth(height/2);
-			seekBar.setThumb(thumb);
-			seekBar.setProgress(1);
-			seekBar.setVisibility(View.VISIBLE);
-			seekBar.setBackgroundColor(Color.BLUE);
+				thumb.setIntrinsicHeight(height);
+				thumb.setIntrinsicWidth(height/2);
+				seekBar.setThumb(thumb);
+				seekBar.setProgress(1);
+				seekBar.setVisibility(View.INVISIBLE);
+				seekBar.setBackgroundColor(Color.BLUE);
+
+				seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
+
+					public void onStopTrackingTouch(SeekBar arg0) {
+						// TODO Auto-generated method stub
+						System.out.println(".....111.......");
+
+					}
+
+					public void onStartTrackingTouch(SeekBar arg0) {
+						// TODO Auto-generated method stub
+						System.out.println(".....222.......");
+					}
+
+					public void onProgressChanged(SeekBar arg0, int arg1, boolean arg2) {
+						// TODO Auto-generated method stub
+						System.out.println(".....333......." + arg1);
+					}
+				});
+				Log.e("VolumeSlider", "created slider");
+			} else {
+				Log.e("VolumeSlider", "updating slider");
+			}
 
 			AbsoluteLayout.LayoutParams lp = new AbsoluteLayout.LayoutParams(width, height, originx, originy);
 			seekBar.setLayoutParams(lp);
-			seekBar.setOnSeekBarChangeListener(new OnSeekBarChangeListener() {
 
-				public void onStopTrackingTouch(SeekBar arg0) {
-					// TODO Auto-generated method stub
-					System.out.println(".....111.......");
-
-				}
-
-				public void onStartTrackingTouch(SeekBar arg0) {
-					// TODO Auto-generated method stub
-					System.out.println(".....222.......");
-				}
-
-				public void onProgressChanged(SeekBar arg0, int arg1, boolean arg2) {
-					// TODO Auto-generated method stub
-					System.out.println(".....333......." + arg1);
-				}
-			});
 		} catch (Exception e) {
-
+			Log.e("VolumeSlider", e.getMessage());
 		}
 	}
 }
